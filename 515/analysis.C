afe4ea130a9bf7a8900eaa73988405c762e6a25f
ManniGertrude <mannigertrude@gmail.com>
2024-05-13 17:25:29 +0200

    modified:   515/4c_HITSvsDT_fixed.pdf

--- 515/analysis.C
+++ 515/analysis.C
@@ -32,7 +32,7 @@ void analysis::Loop()
 //    fChain->GetEntry(jentry);       //read all branches
 //by  b_branchname->GetEntry(ientry); //read only this branch
    //TH2 *Histo = new TH2D("TOT gegen DT", "Ansprecher pro Driftzeit pro Draht", 48, 0.5, 48.5,251,0,627.5);
-   TH1D* Histo = new TH1D("Treffer pro Driftzeit", "asd", 251, 0., 627.5);
+   TH1D* Histo = new TH1D("Treffer pro Driftzeit", "Treffer pro Driftzeit", 251, 0., 627.5);
 
    if (fChain == 0) return;
 
