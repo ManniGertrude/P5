e502694c6782161f7d904821c90ddb027effe1af
ManniGertrude <mannigertrude@gmail.com>
2024-06-08 15:18:13 +0200

    new file:   529/Verschiedene Mats lin.pdf 	new file:   529/Verschiedene Mats log.pdf 	modified:   529/Verschiedene Mats.py

--- 529/Verschiedene Mats.py
+++ 529/Verschiedene Mats.py
@@ -4,39 +4,35 @@ import scipy.odr as odr
 import numpy as np
 
 
-Ordnungszahl = np.array([0, 6, 13, 26, 29, 40, 47])
-Rate = np.array([2627, ])
+OZahl = np.array([0, 6, 13, 26, 29, 40, 47])
+Rate = np.array([2627, 2538, 2403, 1283, 227.1, 340.8, 97.27])
+RateErr = Rate/(10*np.sqrt(30))
+Zr_Rate = np.array([1200, 1185, 1060, 654.3, 113, 206.9, 20.27])
+Zr_RateErr = Zr_Rate/(20*np.sqrt(30))
+Strom = np.array([0.02, 0.02, 0.03, 1, 1, 1, 1])
+StromErr = Strom/50
+
+NormRate = Rate/(Strom)
+NormRateErr = np.sqrt((RateErr/Strom)**2 + (Rate*StromErr/Strom**2)**2)
+
+Zr_NormRate = Zr_Rate/(Strom)
+Zr_NormRateErr = np.sqrt((Zr_RateErr/Strom)**2 + (Zr_Rate*StromErr/Strom**2)**2)
 
 fig, ax= plt.subplots()
  
-def fit(Para, x):
-     return np.exp(-Para[0]*(x + Para[1]) + Para[2])
-
-model = odr.Model(fit)
-mydata = odr.RealData(Dicke,NormRate, sy=NormRateErr)
-myodr = odr.ODR(mydata, model, beta0=[0.0001, 1, 1], maxit=1000)
-out = myodr.run()
-print("para:",out.beta)
-
-yWerte = fit(out.beta, Dicke)
-residuals = Dicke - yWerte
-chisq_odr = np.sum((residuals**2)/NormRate**2)
-ss_res = np.sum(residuals**2)
-ss_tot = np.sum((yWerte-np.mean(yWerte))**2)
-rsquared = 1 - (ss_res / ss_tot)
-# print ('chi^2 =', lin_chisq_odr, 'chi/ndf =', lin_chisq_odr/(len(Strom)-len(lin_out.beta)))
-# print('R^2 =',lin_rsquared)
-
-
-x = np.linspace(np.min(Dicke)-0.2, np.max(Dicke)+0.2, 100)
-y = np.exp(-out.beta[0]*(x+out.beta[1]) + out.beta[2])
-plt.plot(x, y, c="red")
 
 plt.grid()
-plt.errorbar(Dicke, NormRate, xerr=DickeErr, yerr=NormRateErr, color='purple',capsize=2, elinewidth=1.3, capthick=0.8, linestyle='none', label='Messwerte')
+
+plt.errorbar(OZahl, NormRate, xerr=0, yerr=NormRateErr, color='red',capsize=2, elinewidth=1.3, capthick=0.8, linestyle='none', label='Ohne Zr')
+plt.errorbar(OZahl, Zr_NormRate, xerr=0, yerr=Zr_NormRateErr, color='blue',capsize=2, elinewidth=1.3, capthick=0.8, linestyle='none', label='Mit Zr')
 ax.legend()
 plt.yscale('linear')
 plt.savefig("P5\\529\\Verschiedene Mats lin.pdf")
-plt.yscale('log')
+
+def forward(x):
+    return x**4
+def inverse(x):
+    return x**(1/4)
+ax.set_yscale('function', functions=(forward, inverse))
 plt.savefig("P5\\529\\Verschiedene Mats log.pdf")
 plt.show
