f78a4487830223aba7a072a3a9730bbfb38c2901
ManniGertrude <mannigertrude@gmail.com>
2024-06-12 23:35:41 +0200

    modified:   529/Winkel.pdf 	modified:   529/winkel.py

--- 529/winkel.py
+++ 529/winkel.py
@@ -12,7 +12,7 @@ Rate = np.array([1e-4, 1e-4, 1e-4, 2, 300, 1420, 2200, 2300, 2320, 2300, 2250, 1
 RateErr = Rate/(10*np.sqrt(30))+10
 
 def lin_fit(Para, x):
-    return 1/(Para[0]*np.sqrt(2*np.pi)) * np.exp(-Para[0]/2*(Para[2]*(x + Para[1]))**2)
+    return Para[2]/(Para[0]*np.sqrt(2*np.pi)) * np.exp(-1/2*((x - Para[1])/Para[0])**2)
  
 # def lin_fit(Para, x):
 #      return Para[0] * np.cos(Para[2]*(x + Para[1]))**2
@@ -20,7 +20,7 @@ def lin_fit(Para, x):
 
 lin_model = odr.Model(lin_fit)
 lin_mydata = odr.RealData(Winkel, Rate, sx=WinkelErr, sy=RateErr)
-lin_myodr = odr.ODR(lin_mydata, lin_model, beta0=[1, 0.3, 1], maxit=1000)
+lin_myodr = odr.ODR(lin_mydata, lin_model, beta0=[1, 0.3, 2350], maxit=1000)
 lin_out = lin_myodr.run()
 
 x = np.linspace(-3, 3, 100)
@@ -30,10 +30,10 @@ lin_y = lin_fit(lin_out.beta, Winkel)
 lin_residuals = Rate - lin_y
 lin_chisq_odr = np.sum((lin_residuals**2)/Rate**2)
 lin_rsquared = r2_score(Rate, lin_y)
-lin_out.pprint()
+# lin_out.pprint()
 print('$Parameter:', lin_out.beta, lin_out.sd_beta,  '$')
-print ('$\chi_{lin}^2 =', lin_chisq_odr, '\chi/ndf =', lin_chisq_odr/(len(Winkel)-len(lin_out.beta)), '$')
-print('$R_{lin}^2 =',lin_rsquared, '$')
+# print ('$\chi_{lin}^2 =', lin_chisq_odr, '\chi/ndf =', lin_chisq_odr/(len(Winkel)-len(lin_out.beta)), '$')
+# print('$R_{lin}^2 =',lin_rsquared, '$')
 
 plt.plot(x, y, c="red")
 
@@ -41,7 +41,7 @@ plt.plot(x, y, c="red")
 
 
 
-plt.errorbar(Winkel, Rate, yerr=RateErr, color='blue',capsize=4, elinewidth=1.3, capthick=0.8, linestyle='none')
+plt.errorbar(Winkel, Rate,xerr=WinkelErr, yerr=RateErr, color='blue',capsize=2, elinewidth=1.3, capthick=0.8, linestyle='none')
 # ax.legend()
 plt.grid()
 plt.ylabel('Messrate in $s^{-1}$')
