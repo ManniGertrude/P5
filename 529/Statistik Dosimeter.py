186e689b6c65cea86faf7525b4757e7c33da2c45
ManniGertrude <mannigertrude@gmail.com>
2024-06-13 00:33:14 +0200

    new file:   529/Statistik Dosimeter.png 	modified:   529/Statistik Dosimeter.py

--- 529/Statistik Dosimeter.py
+++ 529/Statistik Dosimeter.py
@@ -3,7 +3,7 @@ import scipy.optimize as opt
 import scipy.odr as odr
 import numpy as np
 
-Punkte = 100000
+Punkte = 10000
 
 Radius = 4.5*np.sqrt(np.random.rand(Punkte))
 
@@ -14,22 +14,36 @@ y = Radius * np.sin(Winkel)
 
 xWerte = []
 yWerte = []
-Ungenauigkeit = 1
+
+FxWerte = []
+FyWerte = []
+
+Ungenauigkeit = 2.2
 while len(xWerte)/Punkte < 0.683:
+    print(len(xWerte)/Punkte)
     xWerte = []
     yWerte = []
+    FxWerte = []
+    FyWerte = []
     for i in range(Punkte):
         if x[i] < Ungenauigkeit:
             if x[i] > -Ungenauigkeit:
                 xWerte.append(x[i])
                 yWerte.append(y[i])
-    Ungenauigkeit = Ungenauigkeit + 0.05
+            else:
+                FxWerte.append(x[i])
+                FyWerte.append(y[i])
+        else:
+                FxWerte.append(x[i])
+                FyWerte.append(y[i])
+    Ungenauigkeit = Ungenauigkeit + 0.001
     
     
-    
-plt.scatter(xWerte, yWerte, s=5)
+plt.scatter(FxWerte, FyWerte, s=5, c='b', marker='.')
+plt.scatter(xWerte, yWerte, s=5, c='r', marker='.')
 print(f'Die Messungenauigkeit des Radius beträgt für 1 Sigma etwa {Ungenauigkeit:1f} mm')
 plt.xlim(-5,5)
 plt.ylim(-5,5)
 plt.gca().set_aspect('equal', adjustable='box')
+plt.savefig('P5\\529\\Statistik Dosimeter.png')
 plt.show()
