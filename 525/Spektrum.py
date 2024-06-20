0e559190e3c085c6533f3707d5a8c31db38e7c61
ManniGertrude <mannigertrude@gmail.com>
2024-06-20 13:43:47 +0200

    new file:   525/Messungen/Messung Ba fast links.csv 	new file:   525/Messungen/Messung Ba fast rechts.csv 	new file:   525/Messungen/mcd/Messung Ba fast links.mcd 	new file:   525/Messungen/mcd/Messung Ba fast rechts.mcd 	modified:   525/Spektrum.py

--- 525/Spektrum.py
+++ 525/Spektrum.py
@@ -3,31 +3,35 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit 
 
-LBa1 = pd.read_csv("P5\\525\\Messungen\\Messung links Ba 1.csv", sep="\t",header=0, names=['x', 'y'])
-LBa1Name = ['Ba 1 links']
+# LBa1 = pd.read_csv("P5\\525\\Messungen\\Messung links Ba 1.csv", sep="\t",header=0, names=['x', 'y'])
+# LBa1Name = ['Ba 1 links']
 
-RBa1 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Ba 1.csv", sep="\t",header=0, names=['x', 'y'])
-RBa1Name = ['Ba 1 rechts']
+# RBa1 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Ba 1.csv", sep="\t",header=0, names=['x', 'y'])
+# RBa1Name = ['Ba 1 rechts']
 
-# LNa1 = pd.read_csv("P5\\525\\Messungen\\Messung links Na 1.csv", sep="\t",header=0, names=['x', '0', 'y'])
-# LNa1Name = ['Na 1 links']
+# # LNa1 = pd.read_csv("P5\\525\\Messungen\\Messung links Na 1.csv", sep="\t",header=0, names=['x', '0', 'y'])
+# # LNa1Name = ['Na 1 links']
 
-LNa2 = pd.read_csv("P5\\525\\Messungen\\Messung links Na 2.csv", sep="\t",header=0, names=['x', 'y'])
-LNa2Name = ['Na 2 links']
+# LNa2 = pd.read_csv("P5\\525\\Messungen\\Messung links Na 2.csv", sep="\t",header=0, names=['x', 'y'])
+# LNa2Name = ['Na 2 links']
 
-LNa511 = pd.read_csv("P5\\525\\Messungen\\Messung links Na 511.csv", sep="\t",header=0, names=['x', 'y'])
-LNa511Name = ['Na 511 links']
+# LNa511 = pd.read_csv("P5\\525\\Messungen\\Messung links Na 511.csv", sep="\t",header=0, names=['x', 'y'])
+# LNa511Name = ['Na 511 links']
 
-RNa1 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Na 1.csv", sep="\t",header=0, names=['x', 'y'])
-RNa1Name = ['Na 1 rechts']
+# RNa1 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Na 1.csv", sep="\t",header=0, names=['x', 'y'])
+# RNa1Name = ['Na 1 rechts']
 
-RNa2 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Na 2.csv", sep="\t",header=0, names=['x', 'y'])
-RNa2Name = ['Na 2 rechts']
+# RNa2 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Na 2.csv", sep="\t",header=0, names=['x', 'y'])
+# RNa2Name = ['Na 2 rechts']
 
-RNa511 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Na 511.csv", sep="\t",header=0, names=['x', 'y'])
-RNa511Name = ['Na 511 rechts']
+# RNa511 = pd.read_csv("P5\\525\\Messungen\\Messung rechts Na 511.csv", sep="\t",header=0, names=['x', 'y'])
+# RNa511Name = ['Na 511 rechts']
 
+LfastBa= pd.read_csv("P5\\525\\Messungen\\Messung Ba fast links.csv", sep="\t",header=0, names=['x', 'y'])
+LfastBaName = ['Na 511 rechts']
 
+RfastBa = pd.read_csv("P5\\525\\Messungen\\Messung Ba fast rechts.csv", sep="\t",header=0, names=['x', 'y'])
+RfastBaName = ['Na 511 rechts']
 
 
 
@@ -45,10 +49,10 @@ def gauss(x, H, A, x0, sigma):
 
 ax, plt = plt.subplots()
 def Plot(data, Name):
-    parameters, covariance = curve_fit(gauss, data['x'], data['y']) 
-    fit_y = gauss(data['x'], parameters[0], parameters[1], parameters[2], parameters[3], bounds=([ , , , ], [ , , , ])) 
+    # parameters, covariance = curve_fit(gauss, data['x'], data['y']) 
+    # fit_y = gauss(data['x'], parameters[0], parameters[1], parameters[2], parameters[3], bounds=([ , , , ], [ , , , ])) 
     plt.grid()
-    plt.plot(data['x'], fit_y)
+    # plt.plot(data['x'], fit_y)
     plt.scatter(data['x'], data['y'], color ='cornflowerblue', marker='+', label = Name[0])
     plt.set(xlabel='x', ylabel='y')
     plt.legend()
@@ -57,14 +61,6 @@ def Plot(data, Name):
     plt.cla()
 
 
-
-
-Plot(LBa1, LBa1Name)
-Plot(RBa1, RBa1Name)
-# Plot(LNa1, LNa1Name)
-Plot(LNa2, LNa2Name)
-Plot(RNa1, RNa1Name)
-Plot(RNa2, RNa2Name)
-Plot(LNa511, LNa511Name)
-Plot(RNa511, RNa511Name)
+Plot(LfastBa, LfastBaName)
+Plot(RfastBa, RfastBaName)
 
