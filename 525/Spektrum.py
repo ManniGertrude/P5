5e52ef3c3d106cce11dcda2e5b53677ece4df19c
ManniGertrude <mannigertrude@gmail.com>
2024-06-28 22:41:35 +0200

    modified:   525/Spektrum.py 	modified:   525/pdf/Lebensdauer Fit.pdf 	modified:   525/pdf/Messung Ba 356 links Fit.pdf 	modified:   525/pdf/Messung Ba 81 rechts Fit.pdf 	new file:   525/pdf/Messung Ba fast links 2 Fit nah.pdf 	new file:   525/pdf/Messung Ba fast rechts 2 Fit nah.pdf 	modified:   525/pdf/Messung links Na 2Fit.pdf 	modified:   525/pdf/Messung links Na 511 Fit.pdf 	modified:   525/pdf/Messung rechts Na 2Fit.pdf 	modified:   525/pdf/Messung rechts Na 511 Fit.pdf 	modified:   525/pdf/Na Prompt.pdf

--- 525/Spektrum.py
+++ 525/Spektrum.py
@@ -8,15 +8,15 @@ import scipy.odr as odr
 import math
 from sklearn.metrics import r2_score
 
-# path = "C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\Messungen\\csv" # PC
-path = 'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\Messungen\\csv' # Surface
+path = "C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\Messungen\\csv" # PC
+# path = 'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\Messungen\\csv' # Surface
 
 ax, plt = plt.subplots()
 def Plot(data, Name):
     plt.grid()
     plt.scatter(data['x'], data['y'], color ='cornflowerblue', marker='.', s=2)
     plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
-    ax.savefig(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\pdf\\{Name[:-4]}.pdf')
+    ax.savefig(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\pdf\\{Name[:-4]}.pdf')
     ax.show
     plt.cla()
 
@@ -26,21 +26,19 @@ def gaus(Para, x):
 
 
 
-def gausfit(func, x, y, farbe, beta):
+def gausfit(func, x, y, farbe, beta, Name):
 
     model = odr.Model(func)
     mydata = odr.RealData(x, y)
     myodr = odr.ODR(mydata, model, beta0=beta, maxit=1000)
     out = myodr.run()
     
-    fy = gaus(out.beta, x)
+    fy = func(out.beta, x)
     rsquared = r2_score(y, fy)
     # out.pprint()
     print('$Parameter:', out.beta, out.sd_beta,  '$')
     print('$R_{lin}^2 =',rsquared, '$')
-    print('x', x)
-    print('fy', fy)
-    plt.plot(x, fy)# , c=farbe, label='Lineare Anpassung'
+    plt.plot(x, fy, c=farbe,label = Name )
     return out
 
 
@@ -60,7 +58,7 @@ def gausfit(func, x, y, farbe, beta):
 # # Fünf Dinger in einem
 
 # FünfNamen = ['Messung Na Promt 0ns', 'Messung Na Promt 16ns', 'Messung Na Promt 32ns', 'Messung Na Promt 48ns', 'Messung Na Promt 64ns']
-# FünfFarben = ['darkslategrey', 'mediumorchid', 'firebrick', 'burlywood', 'cornflowerblue']
+# FünfFarben = ['sienna', 'darkgoldenrod', 'darkolivegreen', 'steelblue', 'orchid']
 # LaufAdler = []
 # for i in range(len(FünfNamen)):
 #     fünf = pd.read_csv(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\Messungen\\csv\\{FünfNamen[i]}.csv', sep="\t",header=0, names=['x', 'y'])
@@ -68,8 +66,10 @@ def gausfit(func, x, y, farbe, beta):
 #     plt.scatter(fünf['x'], fünf['y'], color =FünfFarben[i], marker='.', s=2)
 #     plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
 #     beta = [9000, 800, 50]
-#     gausfit(gaus, fünf['x'], fünf['y'], FünfFarben[i], beta)
+#     gausfit(gaus, fünf['x'], fünf['y'], FünfFarben[i], beta, FünfNamen[i][17:])
+#     ax.set_label(FünfNamen)
 # plt.set_xlim(0, 1750)
+# plt.legend()
 # ax.savefig(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\pdf\\Na Prompt.pdf')
 # plt.cla()
 
@@ -88,7 +88,6 @@ def gausfit(func, x, y, farbe, beta):
 #     beta = [50000, 7000, 50]
 #     beta1 = [5000, 3000, 250]
 #     out = gausfit(gaus, fünf['x'][6000:], fünf['y'][6000:], FünfFarben[i+2], beta)
-#     gausfit(gaus, fünf['x'][:6000], fünf['y'][:6000], FünfFarben[i+2], beta1)
 #     out1 = gausfit(gaus, fünf['x'][:6000], fünf['y'][:6000], FünfFarben[i+2], beta1)
 #     plt.plot(fünf['x'], (gaus(out.beta, fünf['x'])+gaus(out1.beta, fünf['x'])), c=FünfFarben[i+2], label='Lineare Anpassung')
 #     ax.savefig(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\pdf\\{FünfNamen[i]}Fit.pdf')
@@ -103,7 +102,7 @@ def gausfit(func, x, y, farbe, beta):
 # FünfFarben = ['orchid', 'navy']
 # LaufAdler = []
 # for i in range(len(FünfNamen)):
-#     fünf = pd.read_csv(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\Messungen\\csv\\{FünfNamen[i]}.csv', sep="\t",header=0, names=['x', 'y'])
+#     fünf = pd.read_csv(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\Messungen\\csv\\{FünfNamen[i]}.csv', sep="\t",header=0, names=['x', 'y'])
 #     plt.grid()
 #     plt.scatter(fünf['x'], fünf['y'], color =FünfFarben[0], marker='.', s=2)
 #     plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
@@ -111,7 +110,7 @@ def gausfit(func, x, y, farbe, beta):
 #     beta = [50000, keke[i] , 50]
 #     out = gausfit(gaus, fünf['x'], fünf['y'], FünfFarben[1], beta)
 #     plt.plot(fünf['x'], gaus(out.beta, fünf['x']), c=FünfFarben[1], label='Lineare Anpassung')
-#     ax.savefig(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\pdf\\{FünfNamen[i]} Fit.pdf')
+#     ax.savefig(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\pdf\\{FünfNamen[i]} Fit.pdf')
 #     plt.cla()
 
 
@@ -135,26 +134,35 @@ def gausfit(func, x, y, farbe, beta):
 
 
 
-# CFD Schwelle
+# # CFD Schwelle
 
-Namen = ['Messung Ba fast links 2', 'Messung Ba fast rechts 2']
-Farbe = ['steelblue', 'goldenrod']
+# Namen = ['Messung Ba fast links 2', 'Messung Ba fast rechts 2']
+# Farbe = ['steelblue', 'goldenrod']
 
-def Erff(Para, x):
-    return Para[0] * special.erf((x - Para[1])/Para[2]+1)
+# def Erff(Para, x):
+#     temp = []
+#     for i in x:
+#         temp2 = Para[0] * (special.erf((i - Para[1])/Para[2])+1)
+#         temp.append(temp2)
+#     return np.array(temp)
      
+# for i in range(2):
+#     data = pd.read_csv(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\Messungen\\csv\\{Namen[i]}.csv', sep="\t",header=0, names=['x', 'y'])
+#     plt.grid()
+#     plt.scatter(data['x'], data['y'], color =Farbe[0], marker='.', s=2, label='Werte')
+#     Grenze = [800, 1300]
+#     Oben = [250, 180]
+#     xWerte = np.linspace(0, Grenze[i], 100)
+#     beta = [70., 400., 1.]
+#     yWerte = Erff(gausfit(Erff, data['x'][:Grenze[i]], data['y'][:Grenze[i]], Farbe[1], beta).beta, xWerte)
+#     plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
+#     plt.legend()
+#     plt.set_xlim(-50, Grenze[i]+50)
+#     plt.set_ylim(-20, Oben[i])
+#     ax.savefig(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\pdf\\{Namen[i]} Fit nah.pdf')
+#     plt.cla()
 
-data = pd.read_csv(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\Messungen\\csv\\{Namen[0]}.csv', sep="\t",header=0, names=['x', 'y'])
-plt.grid()
-plt.scatter(data['x'], data['y'], color =Farbe[0], marker='.', s=2, label='Werte')
-xWerte = np.linspace(0, 1000, 100)
-beta = [1.0, 1.0, 1.0]
-yWerte = gausfit(Erff, data['x'], data['y'], Farbe[1], beta)
-plt.plot(xWerte, yWerte, color =Farbe[1])
-plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
-plt.legend()
-ax.savefig(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\pdf\\{Namen[0]} Fit nah.pdf')
-plt.cla()
+
 
 
 
@@ -164,13 +172,13 @@ def Rfunc(Para, x):
     return Para[0]*np.exp(-Para[1]*(x - Para[2]))
 
 
-# data = pd.read_csv(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\Messungen\\csv\\Messung Lebensdauer.csv', sep="\t",header=0, names=['x', 'y'])
-# plt.grid()
-# beta = [120, 1e-3, 1500]
-# out = gausfit(Rfunc, data['x'][1500:3000], data['y'][1500:3000], 'firebrick', beta)
-# plt.plot(data['x'][1500:3000], Rfunc(out.beta, data['x'][1500:3000]), c='firebrick', label='Lineare Anpassung')
-# plt.scatter(data['x'][1000:3000], data['y'][1000:3000], color='darkslategrey', marker='.', s=2)
-# plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
-# # plt.set_xlim(0, 1750)
-# ax.savefig(f'C:\\Users\\Surface Pro 7 Manni\\Desktop\\Code Dateien\\P5\\525\\pdf\\Lebensdauer Fit.pdf')
-# plt.cla()
+data = pd.read_csv(f'C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\Messungen\\csv\\Messung Lebensdauer.csv', sep="\t",header=0, names=['x', 'y'])
+plt.grid()
+beta = [120, 1e-3, 1500]
+out = gausfit(Rfunc, data['x'][1500:3000], data['y'][1500:3000], 'firebrick', beta, 'Lebenszeitkurve')
+plt.scatter(data['x'][1000:3000], data['y'][1000:3000], color='indigo', marker='.', s=2)
+plt.set(xlabel='Kanalnummer', ylabel='Anzahl an Messergebnissen')
+# plt.set_xlim(0, 1750)
+plt.legend()
+ax.savefig('C:\\Users\\kontr\\Desktop\\Github\\P5\\525\\pdf\\Lebensdauer Fit.pdf')
+plt.cla()
